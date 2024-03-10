b060630174a18e30ddb5679e4e074e293231ca00
Matthew Morikan <matthew@Phobos.local>
2024-03-09 16:30:38 -0800

    Fixed typo

--- DoodleDuel/MenuView.swift
+++ DoodleDuel/MenuView.swift
@@ -24,7 +24,7 @@ struct MenuView: View {
             Spacer()
             
             Button {
-                matchManger.startMatchMaking()
+                matchManager.startMatchMaking()
             } label: {
                 Text("PLAY")
                     .font(.largeTitle)
