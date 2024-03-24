e0c5862faa391fb7715d47078ba941a5e41fc527
Angad88 <121978333+Angad88@users.noreply.github.com>
2024-03-24 16:19:42 -0700

    Extra Changes

--- DoodleDuel/MIsc.swift
+++ DoodleDuel/MIsc.swift
@@ -8,15 +8,15 @@
 
 import Foundation
 
-let everydayObjects = ["pen", "paper", "book", "table", "chair", "computer", "phone", "keys", "wallet", "watch", "clock", "lamp", "bed", "pillow", "blanket", "sofa", "tv", "remote", "car", "bicycle", "bus", "train", "plane", "umbrella", "sunglasses", "shoe", "pants", "shirt", "hat", "coat", "gloves", "scarf", "socks", "toothbrush", "toothpaste", "soap", "shampoo", "conditioner", "razor", "towel", "dish", "silverware", "glass", "plate", "microwave", "fridge", "stove", "oven", "dishwasher", "washing machine", "dryer", "vacuum cleaner", "mop", "broom", "dustpan", "trash can", "laundry basket", "hanger", "iron", "hairbrush", "comb", "nail clipper", "scissors", "tape", "glue", "stapler", "paperclip", "binder", "folder", "envelope", "post-it note", "calendar", "whiteboard", "marker", "eraser", "pencil", "ruler", "compass", "protractor", "calculator", "flashlight", "battery", "extension cord", "power strip", "plunger", "screwdriver", "hammer", "wrench", "tape measure", "level", "pliers", "saw"]
+let everydayObjects = ["pen", "paper", "book", "table", "chair", "computer", "phone", "keys", "wallet", "watch","computer","mouse","laptop","spider","ant","icecream","pizza","banana","cat","dog","desk","sun","table fan", "clock", "lamp", "bed", "pillow", "blanket", "sofa", "tv", "remote", "car", "bicycle", "bus", "train", "plane", "umbrella", "sunglasses", "shoe", "pants", "shirt", "hat", "coat", "gloves", "scarf", "socks", "toothbrush", "toothpaste", "soap", "shampoo", "conditioner", "razor", "towel", "dish", "silverware", "glass", "plate", "microwave", "fridge", "stove", "oven", "dishwasher", "washing machine", "dryer", "vacuum cleaner", "broom", "dustpan", "trash can", "hanger", "hairbrush", "comb", "nail clipper", "scissors", "tape", "glue", "stapler", "paperclip", "binder", "folder", "envelope", "post-it note", "calendar", "whiteboard", "marker", "eraser", "pencil", "ruler", "compass", "protractor", "calculator", "flashlight", "battery", "plunger", "screwdriver", "hammer", "wrench", "tape measure", "pliers"]
 
 enum PlayerAuthState: String {
-    case authenticating = "Logging in to Game Center..."
-    case unauthenticated = "Please sign in to Game Center to play."
+    case authenticating = "Loading: Logging in to Game Center..."
+    case unauthenticated = "Please sign in to Game Center to play DoodleDuel."
     case authenticated = ""
     
-    case error = "There was an error logging into Game Center."
-    case restricted = "You're not allowed to play multiplayer games!"
+    case error = "Error: There was a problem logging into Game Center."
+    case restricted = "Your account is not allowed to play multiplayer games."
 }
 
 struct PastGuess: Identifiable {
